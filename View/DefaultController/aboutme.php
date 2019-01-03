726248848bab973cea5f2f47669d5a208af25ec6
JanSzewczyk <jan.szewczyk1997@gmail.com>
2019-01-03 21:03:26 +0100

    graphic

---
+++ View/DefaultController/aboutme.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html >
+
+<head>
+    <meta charset="UTF-8">
+    <title>fishbook</title>
+    <script type="text/javascript" src="View/DefaultController/Homepage/timer.js"></script>
+    <link rel="stylesheet" href="View/DefaultController/Homepage/Fontello/css/fontello.css" type="text/css"/>
+    <link rel="stylesheet" href="View/DefaultController/Homepage/style.css" type="text/css"/>
+</head>
+
+<body onload="odliczanie();">
+    <div id="container">
+        <div class="rectangle">
+            <a href="?page=index" class="tilelink">
+                <div id="logo">
+                    <img src="Resources/Homepage/logo.png">
+                </div>
+            </a>
+            <div id="zegar"></div>
+            <div style="clear: both"></div>
+        </div>
+
+        <div class="square">
+            <a href="?page=login" class="tilelink">
+                <div class="login">
+                    <i class="icon-user"></i></br>Zaloguj się
+                </div>
+            </a>
+            <a href="?page=funny" class="tilelink">
+                <div class="funnyzone">
+                    <i class="icon-chat-inv"></i></br>Funny zone
+                </div>
+            </a>
+            <div style="clear: both"></div>
+
+            <a href="?page=signup" class="tilelink">
+                 <div class="signup">
+                     <i class="icon-user-plus"></i></br>Zarejestruj się
+                 </div>
+            </a>
+            <a href="?page=aboutme" class="tilelink">
+                 <div class="aboutme">
+                    <i class="icon-heart"></i></br>O projekcie
+                </div>
+            </a>
+            <div style="clear: both"></div>
+
+            <div class="title4">
+                <i class="icon-users"></i> Dołącz do NAS
+            </div>
+            <a href="http://instagram.com" target="_blank" class="tilelink">
+                <div class="ig">
+                    <i class="icon-instagram-filled"></i>
+                </div>
+            </a>
+            <div style="clear: both"></div>
+        </div>
+        <div class="square">
+            <div class="title5">
+                Projekt zaliczeniowy z przedmiotu: <br>
+                Projektowanie Aplikacji Internetowych <br><br>
+                Autor : Jan Robert Szewczyk
+
+            </div>
+            <a href="http://youtube.com" target="_blank" class="tilelink">
+                <div class="yt">
+                    <i class="icon-youtube"></i>
+                </div>
+            </a>
+            <a href="http://facebook.com" target="_blank" class="tilelink">
+                <div class="fb">
+                    <i class="icon-facebook"></i>
+                </div>
+            </a>
+            <a href="http://plus.google.com" target="_blank" class="tilelink">
+                <div class="gplus">
+                    <i class="icon-gplus"></i>
+                </div>
+            </a>
+            <a href="http://twitter.com" target="_blank" class="tilelink">
+                <div class="tw">
+                    <i class="icon-twitter-2"></i>
+                </div>
+            </a>
+        </div>
+        <div style="clear: both"></div>
+
+        <div class="rectangle">2019 &copy fishbook created by Jan Szewczyk </div>
+    </div>
+
+</body>
+</html>
---
+++ View/DefaultController/funny.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html >
+
+<head>
+    <meta charset="UTF-8">
+    <title>fishbook</title>
+    <script type="text/javascript" src="View/DefaultController/Homepage/timer.js"></script>
+    <link rel="stylesheet" href="View/DefaultController/Homepage/Fontello/css/fontello.css" type="text/css"/>
+    <link rel="stylesheet" href="View/DefaultController/Homepage/style.css" type="text/css"/>
+</head>
+
+<body onload="odliczanie();">
+    <div id="container">
+        <div class="rectangle">
+            <a href="?page=index" class="tilelink">
+                <div id="logo">
+                    <img src="Resources/Homepage/logo.png">
+                </div>
+            </a>
+            <div id="zegar"></div>
+            <div style="clear: both"></div>
+        </div>
+
+        <div class="square">
+            <a href="?page=login" class="tilelink">
+                <div class="login">
+                    <i class="icon-user"></i></br>Zaloguj się
+                </div>
+            </a>
+            <a href="?page=funny" class="tilelink">
+                <div class="funnyzone">
+                    <i class="icon-chat-inv"></i></br>Funny zone
+                </div>
+            </a>
+            <div style="clear: both"></div>
+
+            <a href="?page=signup" class="tilelink">
+                 <div class="signup">
+                     <i class="icon-user-plus"></i></br>Zarejestruj się
+                 </div>
+            </a>
+            <a href="?page=aboutme" class="tilelink">
+                 <div class="aboutme">
+                    <i class="icon-heart"></i></br>O projekcie
+                </div>
+            </a>
+            <div style="clear: both"></div>
+
+            <div class="title4">
+                <i class="icon-users"></i> Dołącz do NAS
+            </div>
+            <a href="http://instagram.com" target="_blank" class="tilelink">
+                <div class="ig">
+                    <i class="icon-instagram-filled"></i>
+                </div>
+            </a>
+            <div style="clear: both"></div>
+        </div>
+        <div class="square">
+            <div class="title5">
+                Mój stary to fanatyk wędkarstwa.<br><br>
+                Mój stary to fanatyk wędkarstwa. Pół mieszkania zaj***ne wędkami najgorsze.
+                Średnio raz w miesiącu ktoś wdepnie<br/>
+                w leżący na ziemi haczyk czy kotwicę i trzeba wyciągać <br/>
+                w szpitalu bo mają zadziory na końcu.
+                W swoim 22 letnim życiu już z 10 razy byłem na takim zabiegu. Tydzień temu poszedłem na jakieś losowe badania to baba z recepcji
+                jak mnie tylko zobaczyła to kazała buta ściągać xD bo myślała, że znowu hak<br/>
+                w nodze.<br>
+                Druga połowa mieszkania zaj***na Wędkarzem Polskim, ... <br><br>
+                Jesteś ciekawy co działo się dalej ??
+                <br>-> <a href="http://pl.copypasta.wikia.com/wiki/M%C3%B3j_stary_to_fanatyk_w%C4%99dkarstwa." target="_blank">Pasta o wędkarzu.</a>
+            </div>
+            <a href="http://youtube.com" target="_blank" class="tilelink">
+                <div class="yt">
+                    <i class="icon-youtube"></i>
+                </div>
+            </a>
+            <a href="http://facebook.com" target="_blank" class="tilelink">
+                <div class="fb">
+                    <i class="icon-facebook"></i>
+                </div>
+            </a>
+            <a href="http://plus.google.com" target="_blank" class="tilelink">
+                <div class="gplus">
+                    <i class="icon-gplus"></i>
+                </div>
+            </a>
+            <a href="http://twitter.com" target="_blank" class="tilelink">
+                <div class="tw">
+                    <i class="icon-twitter-2"></i>
+                </div>
+            </a>
+        </div>
+        <div style="clear: both"></div>
+
+        <div class="rectangle">2019 &copy fishbook created by Jan Szewczyk </div>
+    </div>
+
+</body>
+</html>
